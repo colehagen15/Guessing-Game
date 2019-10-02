91db1daa23e9f4877eda0929303ef4c936397510
colehagen15 <54552528+colehagen15@users.noreply.github.com>
2019-10-02 14:23:04 -0500

    Added jQuery Elements

--- guessingGame.js
+++ guessingGame.js
@@ -6,76 +6,72 @@ var guesses = [];
 
 console.log(answer); //Dubugging purposes
 
-//Setting Elements
-let countElement = document.getElementById("count");
-let restartBtn = document.getElementById("restart");
-let checkBtn = document.getElementById("check");
-let cheatBtn = document.getElementById("cheat");
-let pastGuesses = document.getElementById("pastGuesses");
-let feedBack = document.getElementById("demo");
-let guessList = document.getElementById("guessList");
-let userGuesses = document.getElementById("userGuess");
-let image = document.getElementById("img");
+//Setting Elements in jQuery
+let countElement = $("#count");
+let restartBtn = $("#restart");
+let checkBtn = $("#check");
+let cheatBtn = $("#cheat");
+let pastGuesses = $("#pastGuesses");
+let feedBack = $("#demo");
+let guessList = $("#guessList");
+let userGuesses = $("#userGuess");
+let image = $("#img");
 
-image.style.display="none";
-countElement.style.display= "none";
-restartBtn.style.visibility="hidden";
+image.hide();;
+countElement.hide();
+restartBtn.hide();
 
 //Checks guesses and tells user if low, high, or correct
 function checkGuess() {
     console.log("You clicked the button");
     console.log(answer);
-    countElement.style.display= "block";
-    var userGuess = userGuesses.value;
+    countElement.show();
+    var userGuess = userGuesses.val();
     guesses.push(userGuess);
     console.log(guesses);
     console.log(userGuess);
-    feedBack.style.visibility="hidden";
-    image.setAttribute("src", "loadingGif.gif");
-    image.style.display="inline"; //Might need to be inline
-    checkBtn.disabled=true;
+    feedBack.hide();
+    image.show(); //Might need to be inline
+    checkBtn.attr("disabled", true);
     setTimeout(() => {
-        image.style.display="none";
-        feedBack.style.visibility="visible";
+        image.hide();
+        feedBack.show();
         if (userGuess < answer && userGuess >= 0) {
-            feedBack.innerHTML ="Guess is too low";
+            feedBack.text("Guess is too low");
         }
         else if (userGuess > answer && userGuess <= 20) {
-            feedBack.innerHTML ="Guess is too high";
+            feedBack.text("Guess is too high");
         }
          else if (userGuess == answer) {
-            feedBack.innerHTML ="You guessed it! You won the game";
-            userGuesses.style.visibility="hidden";
-            restartBtn.style.visibility="visible";
-            //document.getElementById("check").style.visibility="hidden";
-            //document.getElementById("guessList").style.visibility="hidden";
-            //document.getElementById("cheatBtn").style.visibility="hidden";
+            feedBack.text("You guessed it! You won the game");
+            userGuesses.hide();
+            restartBtn.show();
         }
         else if (userGuess > 20 || userGuess < 0){
-        feedBack.innerHTML="Guess is out of range, must be between 0 and 20!";
+        feedBack.text("Guess is out of range, must be between 0 and 20!");
         }
         else if (isNaN(userGuess)) {
-            feedBack.innerHTML = "Guess must be a number. Try again."
+            feedBack.text("Guess must be a number. Try again.");
         }
         count ++;
-        countElement.innerHTML='Attempt: ' + count;
+        countElement.text('Attempt: ' + count);
 
-        checkBtn.disabled=false;
+        checkBtn.attr("disabled",false);
     }, 3000)
 }
 
 //Shows the user past guesses
 function showList() {
     console.log(guesses);
-    pastGuesses.style.visibility="visible";
-    pastGuesses.innerHTML = "Guess History: " + guesses; 
+    pastGuesses.show();
+    pastGuesses.text("Guess History: " + guesses); 
 }
 
 //Shows user answer 
 function cheat() {
     console.log(answer);
-    cheatBtn.style.visibility="visible";
-    cheatBtn.innerHTML = "Answer is: " + answer;
+    cheatBtn.show();
+    cheatBtn.text("Answer is: " + answer);
 }
 
 //Unhides every element hidden from win
@@ -83,12 +79,12 @@ function restart() {
     answer = Math.floor(Math.random() * 20);
     count = 0;
     guesses = [];
-    userGuesses.style.visibility="visible";
-    cheatBtn.style.visibility="hidden";
-    restartBtn.style.visibility="hidden";
-    feedBack.style.visibility="hidden";
-    checkBtn.style.visibility="visible";
-    cheatBtn.style.visibility="hidden";
-    pastGuesses.style.visibility="hidden";
-    document.getElementById("count").innerHTML = 'Attempt: ' + count;
+    userGuesses.show();
+    cheatBtn.hide();
+    restartBtn.hide();
+    feedBack.hide();
+    checkBtn.show();
+    cheatBtn.hide();
+    pastGuesses.hide();
+    countElement.hide();
 }
